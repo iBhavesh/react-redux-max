680d371eaf607d2b4010e4c62c2c8e756200b5a7
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-05-11 18:38:15 +0000

    Refactor Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,23 +13,21 @@ const Header = () => {
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          {isAuthenticated && (
-            <>
-              <li>
-                <a href="/">My Products</a>
-              </li>
-              <li>
-                <a href="/">My Sales</a>
-              </li>
-              <li>
-                <button onClick={logoutHandler}>Logout</button>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
+      {isAuthenticated && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
